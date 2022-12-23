6f020e167644b921c3c3745cae95157875712d23
MariaSoroks <sormash@gmail.com>
2022-12-23 14:05:37 +0600

    Fixing...

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a = number/100;
+        int b = (number%100)/10;
+        int c = (number%100)%10;
+        int invert = c*100 + b*10 + a;
+        System.out.println(invert);
     }
 }
