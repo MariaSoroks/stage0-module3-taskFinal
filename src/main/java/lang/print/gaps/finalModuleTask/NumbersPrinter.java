07bb04643d819b27473fbf96758ab58d3e5ac984
MariaSoroks <sormash@gmail.com>
2022-12-23 14:08:46 +0600

    Fixinggg...

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        boolean bl = number >= 0;
+        boolean bl = number > 0;
         System.out.println(bl);
     }
 }
